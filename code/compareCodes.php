6c9639fd6efacd2e7ed24aad63ff59c428d4d46d
Jason Dinkelmann <jason@aboutgolf.com>
2015-03-18 16:05:11 -0400

    jkjkh

---
+++ code/compareCodes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jdinkelmann
+ * Date: 3/13/15
+ * Time: 9:15 AM
+ */ 
---
+++ code/createCodes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jdinkelmann
+ * Date: 3/2/15
+ * Time: 7:48 PM
+ */
+
+class createCodes {
+
+} 
