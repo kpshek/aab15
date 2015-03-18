05e6cecf817ade3d8fe09fe336bd4991efa85ed5
Jason Dinkelmann <jason@aboutgolf.com>
2015-03-18 16:05:49 -0400

    jkjkh

--- code/compareCodes.php
+++ code/compareCodes.php
@@ -4,4 +4,11 @@
  * User: jdinkelmann
  * Date: 3/13/15
  * Time: 9:15 AM
- */ 
+ */
+ini_set('auto_detect_line_endings', true);
+$file = fopen('ford.txt', 'r');
+$usedCodes = fopen('used_codes.csv', 'r');
+
+while (($line = fgetcsv($file)) !== FALSE) {
+
+}
--- code/createCodes.php
+++ code/createCodes.php
@@ -5,7 +5,19 @@
  * Date: 3/2/15
  * Time: 7:48 PM
  */
+ini_set('auto_detect_line_endings', true);
+$file = fopen('medum.csv', 'r');
+while (($line = fgetcsv($file)) !== FALSE) {
+    //$line is an array of the csv elements
+    //$name = $line[1];
 
-class createCodes {
+    //$names = explode(' ',$name);
+    $firstName = $line[0];
+    $lastName = $line[1];
+    $email = $line[2];
+    $company = 'UMMED';
+    $code = md5($email);
 
-} 
+    print $firstName.",".$lastName.",".$email.",".$company.$code."<br>";
+}
+fclose($file);
